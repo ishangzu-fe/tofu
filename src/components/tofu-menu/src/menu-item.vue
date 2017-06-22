a9c83e2512a8a7bb2418ddc22c532140f1398a94
wchen <406653843@qq.com>
2017-06-22 18:21:04 +0800

    fix: menubar visual bug

--- src/components/tofu-menu/src/menu-item.vue
+++ src/components/tofu-menu/src/menu-item.vue
@@ -2,7 +2,7 @@
     <div class="menu-item">
         <div
             class="menu-item-main"
-            :class="{'menu-item-active': (activeMenu === menu) || (childrenVisibility)}"
+            :class="{'menu-item-active': activeMenu === menu}"
             :style="{'padding-left': paddingLeft}"
             @click.stop="activate">
             <i class="menu-item-icon" v-if="menu.iconClass || menu.imgSrc">
@@ -10,7 +10,6 @@
             </i>
             <span class="menu-item-icon" v-else-if="menu._deep === 0"><slot></slot></span>
             <span class="menu-item-label">{{ menu.label }}</span>
-            <!--<span class="menu-item-arrow" :class="{'menu-item-arrow-active': childrenVisibility}" v-if="menu.children"></span>-->
             <span class="menu-item-sign"></span>
         </div>
         <menu-item
@@ -24,6 +23,99 @@
         </menu-item>
     </div>
 </template>
+
+<script>
+import VIcon from '../../tofu-icon'
+
+export default {
+    name:'menu-item',
+
+    components: {
+        VIcon
+    },
+
+    props:{
+        useRouter: {
+            default: false
+        },
+        menu: {
+            required: true,
+        },
+        activeMenu: {
+            required: true
+        }
+    },
+
+    data () {
+        return {
+            childrenVisibility: false,
+            visible: this.menu._visible
+        }
+    },
+
+    computed: {
+        paddingLeft () {
+            return this.menu._deep * 32 + 24 + 'px' // TODO 有两个未知问题？
+        }
+    },
+
+    methods: {
+        activate () {
+            if (this.menu.children) {
+                this.childrenVisibility = !this.childrenVisibility;
+            } else {
+                // 如果使用 vue-router，则 push 路径
+                // 否则就执行指定的函数
+                if (this.useRouter) {
+                    this.$router.push(this.menu.path)
+                }
+                // else {}
+
+                // 触发事件
+                this.$emit('menuItem_activate', this.menu)
+            }
+        },
+
+        emitSubActivate (menu) {
+            this.$emit('menuItem_activate', menu)
+        },
+
+        checkVisibility (v) {
+            let parents = []
+            const extractParents = (v) => {
+                if (v._parent) {
+                    parents.push(v._parent._id)
+                    if (v._parent._parent) {
+                        extractParents(v._parent)
+                    }
+                }
+            }
+
+            extractParents(v);
+
+            return parents.indexOf(this.menu._id) !== -1
+        }
+    },
+
+    created () {
+        // 如果是父菜单，自动切换子菜单的可见性
+        if (this.useRouter && this.menu.children) {
+            this.$watch('activeMenu', (newV) => {
+                if (newV) {
+                    this.childrenVisibility = this.checkVisibility(newV)
+                } else {
+                    this.childrenVisibility = false
+                }
+            })
+
+            // 初始化时
+            if (this.activeMenu && this.checkVisibility(this.activeMenu)) {
+                this.childrenVisibility = true
+            }
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .menu-item {
@@ -91,26 +183,6 @@
             text-align: center;
         }
 
-        // .menu-item-arrow {
-        //     position: absolute;
-        //     right: 16px;
-        //     top: 10px;
-
-        //     width: 16px;
-        //     height: 16px;
-
-        //     background: url('/static/triangle_down.svg') no-repeat;
-        //     background-position: center;
-        //     background-size: 8px 8px;
-
-        //     transform: rotate(-90deg);
-        //     transition: transform .2s ease-in-out;
-
-        //     &.menu-item-arrow-active {
-        //         transform: rotate(0deg)
-        //     }
-        // }
-
         .menu-item-sign {
             position: absolute;
             left: 0;
@@ -154,108 +226,3 @@
     }
 }
 </style>
-
-<script>
-    import VIcon from '../../tofu-icon'
-
-    export default {
-        name:'menu-item',
-
-        components: {
-            VIcon
-        },
-
-        props:{
-            useRouter: {
-                default: false
-            },
-            menu: {
-                required:true,
-                validator () {
-                    return true
-                    // TODO 校验
-                    // {
-                    //   label,
-                    //   name,
-                    //   id,
-                    //   idx,
-                    //   children,
-                    //   activate
-                    // }
-                }
-            },
-            activeMenu: {
-                required: true
-            }
-        },
-
-        data () {
-            return {
-                childrenVisibility: false,
-                visible: this.menu._visible
-            }
-        },
-
-        computed: {
-            paddingLeft () {
-                return this.menu._deep * 32 + 24 + 'px' // TODO 有两个未知问题？
-            }
-        },
-
-        methods: {
-            activate () {
-                if (this.menu.children) {
-                    this.childrenVisibility = !this.childrenVisibility;
-                } else {
-                    // 如果使用 vue-router，则 push 路径
-                    // 否则就执行指定的函数
-                    if (this.useRouter) {
-                        this.$router.push(this.menu.path)
-                    }
-                    // else {}
-
-                    // 触发事件
-                    this.$emit('menuItem_activate', this.menu)
-                }
-            },
-
-            emitSubActivate (menu) {
-                this.$emit('menuItem_activate', menu)
-            },
-
-            checkVisibility (v) {
-                let parents = []
-                const extractParents = (v) => {
-                    if (v._parent) {
-                        parents.push(v._parent._id)
-                        if (v._parent._parent) {
-                            extractParents(v._parent)
-                        }
-                    }
-                }
-
-                extractParents(v);
-
-                return parents.indexOf(this.menu._id) !== -1
-            }
-        },
-
-        created () {
-            // 如果是父菜单，自动切换子菜单的可见性
-            if (this.useRouter && this.menu.children) {
-                this.$watch('activeMenu', (newV) => {
-                    if (newV) {
-                        this.childrenVisibility = this.checkVisibility(newV)
-                    } else {
-                        this.childrenVisibility = false
-                    }
-                })
-
-                // 初始化时
-                if (this.activeMenu && this.checkVisibility(this.activeMenu)) {
-                    this.childrenVisibility = true
-                }
-            }
-        }
-    }
-</script>
