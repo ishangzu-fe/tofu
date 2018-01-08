05a621cde1b3af1e17382edd7026921bd8d48d1a
wchen <406653843@qq.com>
2018-01-08 11:32:04 +0800

    修复 table 中 checkbox 的问题

--- bin/publish.js
+++ bin/publish.js
@@ -1,4 +1,22 @@
 const { execSync } = require('child_process')
+
+const registry = execSync('npm config get registry', {
+    encoding: 'utf-8'
+})
+if (!~registry.indexOf('https://registry.npmjs.org')) {
+    throw new Error(`仓库地址设置错误，当前仓库为：${registry}`)
+}
+const isWorkingTreeClean = !execSync('git status --porcelain', { encoding: 'utf-8' })
+    .trim()
+    .split('\n')
+    .filter((line) => {
+        return line.trim() && !line.match(/^\?\? /)
+    }).map((line) => {
+        return line.trim()
+    }).length
+
+if (!isWorkingTreeClean) throw new Error('请先提交未提交的内容')
+
 const packageJSON = require('../package.json')
 const semver = require('semver')
 const inquirer = require('inquirer')
@@ -23,5 +41,5 @@ inquirer.prompt({
     name: 'confirm',
     message: `确定发布版本 v${newVersion} 吗？`
 }).then(res => {
-    if (res.confirm) execSync(`npm version ${newVersion} -f --allow-same-version && npm publish`)
+    if (res.confirm) execSync(`npm version ${newVersion} --allow-same-version && npm publish`)
 }).catch(_ => {})
