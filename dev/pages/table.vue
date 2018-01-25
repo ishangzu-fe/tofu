de0c4d57bd7520f44afc17e8640475ceb4fb3f35
hujin0327 <hujin0327@126.com>
2018-01-25 17:28:17 +0800

    隐藏table组件fixed时.gutter的占位符

--- dev/pages/table.vue
+++ dev/pages/table.vue
@@ -4,10 +4,23 @@
             <i-table :data="tableData1" style="width:100%">
                 <i-table-column prop="date" label="日期" width="180"></i-table-column>
                 <i-table-column prop="name" label="姓名" width="180"></i-table-column>
-                <i-table-column prop="address" label="地址" width="100" show-tooltip-when-overflow></i-table-column>
-                
+                <i-table-column prop="address" label="地址" show-tooltip-when-overflow></i-table-column>
             </i-table> 
         </demo-block>
+        <demo-block title="带斑马纹表格">
+            <i-table :data="tableData1" stripe style="width:100%">
+                <i-table-column prop="date" label="日期" width="180"></i-table-column>
+                <i-table-column prop="name" label="姓名" width="180"></i-table-column>
+                <i-table-column prop="address" label="地址"></i-table-column>
+            </i-table>
+        </demo-block>  
+        <demo-block title="带边框表格">
+            <i-table :data="tableData1" border style="width:100%">
+                <i-table-column prop="date" label="日期" width="180"></i-table-column>
+                <i-table-column prop="name" label="姓名" width="180"></i-table-column>
+                <i-table-column prop="address" label="地址"></i-table-column>
+            </i-table>
+        </demo-block>  
     </div>
 </template>
 
