10c3d0db167a4a04de5e9933208b40530baf420d
钱张盛 <qianzhangsheng@hztianque.com>
2019-04-15 14:30:56 +0800

    删除无用路由

--- dev/router/routes.js
+++ dev/router/routes.js
@@ -1,4 +1,3 @@
-import Hello from '../pages/hello'
 import Alert from '../pages/alert'
 import Affix from '../pages/affix'
 import AutoComplete from '../pages/autocomplete'
@@ -29,25 +28,9 @@ import publicPage from '../pages/publicpage'
 import ChangeLog from '../pages/changelog.md'
 import Installation from '../pages/installation.md'
 import Quickstart from '../pages/quickstart.md'
-import Icon from '../pages/hello.vue'
 import Container from '../pages/layout'
 import DateTimePicker from '../pages/dateTimePicker.vue'
-import InputNumber from '../pages/hello.vue'
-import Rate from '../pages/hello.vue'
-import Slider from '../pages/hello.vue'
 import Transfer from '../pages/transfer.vue'
-import Upload from '../pages/hello.vue'
-import Carousel from '../pages/hello.vue'
-import Collapse from '../pages/hello.vue'
-import Tag from '../pages/hello.vue'
-import Breadcrumb from '../pages/hello.vue'
-import Dropdown from '../pages/hello.vue'
-import NavMenu from '../pages/hello.vue'
-import Pagination from '../pages/hello.vue'
-import Steps from '../pages/hello.vue'
-import Notification from '../pages/hello.vue'
-import Progress from '../pages/hello.vue'
-import Loading from '../pages/hello.vue'
 
 let routes = [
     { path: '/changelog', component: ChangeLog, name: '更新日志' },
@@ -66,8 +49,7 @@ let routes = [
         name: 'Basic',
         children: [
             { path: '/layout', component: Layout, name: '布局' },
-            { path: '/container', component: Container, name: '布局容器' }, // 暂缺
-            { path: '/icon', component: Icon, name: '图标' }, // 暂缺
+            { path: '/container', component: Container, name: '布局容器' },
             { path: '/button', component: Button, name: '按钮' },
         ],
     },
@@ -79,17 +61,13 @@ let routes = [
             { path: '/radio', component: Radio, name: '单选框' },
             { path: '/checkbox', component: Checkbox, name: '多选框' },
             { path: '/input', component: Input, name: '输入框' },
-            { path: '/inputNumber', component: InputNumber, name: '计数器' }, // 暂缺
             { path: '/select', component: Select, name: '选择器' },
             { path: '/cascader', component: Cascader, name: '级联选择' },
             { path: '/switch', component: Switch, name: '开关' },
-            { path: '/slider', component: Slider, name: '滑块' }, // 暂缺
             { path: '/timePicker', component: TimePicker, name: '时间选择器' },
             { path: '/datePicker', component: DatePicker, name: '日期选择框' },
-            { path: '/dateTimePicker', component: DateTimePicker, name: '日期时间选择框' }, // 暂缺
-            { path: '/upload', component: Upload, name: '上传' }, // 暂缺
-            { path: '/rate', component: Rate, name: '评分' }, // 暂缺
-            { path: '/transfer', component: Transfer, name: '穿梭框' }, // 暂缺
+            { path: '/dateTimePicker', component: DateTimePicker, name: '日期时间选择框' },
+            { path: '/transfer', component: Transfer, name: '穿梭框' },
             { path: '/form', component: Form, name: '表单' },
             { path: '/autocomplete', component: AutoComplete, name: '自动完成' },
         ],
@@ -100,11 +78,8 @@ let routes = [
         name: 'Data',
         children: [
             { path: '/table', component: Table, name: '表格' },
-            { path: '/tag', component: Tag, name: '标签' }, // 暂缺
-            { path: '/progress', component: Progress, name: '进度条' }, // 暂缺
             { path: '/dropdown-tree', component: DropdownTree, name: '树选择（现役）' },
             { path: '/select-tree', component: SelectTree, name: '树选择' },
-            { path: '/pagination', component: Pagination, name: '分页' }, // 暂缺
             { path: '/badge', component: Badge, name: '标记' },
         ],
     },
@@ -114,10 +89,8 @@ let routes = [
         name: 'Notic',
         children: [
             { path: '/alert', component: Alert, name: '警告' },
-            { path: '/loading', component: Loading, name: '加载' }, // 暂缺
             { path: '/message', component: Message, name: '消息提示' },
             { path: '/message-box', component: MessageBox, name: '弹框' },
-            { path: '/notification', component: Notification, name: '通知' }, // 暂缺
         ],
     },
     {
@@ -125,11 +98,7 @@ let routes = [
         component: publicPage,
         name: 'Navigation',
         children: [
-            { path: '/navmenu', component: NavMenu, name: '导航菜单' }, // 暂缺
             { path: '/tabs', component: Tabs, name: '标签页' },
-            { path: '/breadcrumb', component: Breadcrumb, name: '面包屑' }, // 暂缺
-            { path: '/dropdown', component: Dropdown, name: '下拉' }, // 暂缺
-            { path: '/steps', component: Steps, name: '步骤条' }, // 暂缺
         ],
     },
     {
@@ -139,8 +108,6 @@ let routes = [
         children: [
             { path: '/dialog', component: Dialog, name: '对话框' },
             { path: '/card', component: Card, name: '卡片' },
-            { path: '/carousel', component: Carousel, name: '走马灯' }, // 暂缺
-            { path: '/collapse', component: Collapse, name: '折叠面板' }, // 暂缺
             { path: '/imgList', component: ImgList, name: '图片列表' },
             { path: '/affix', component: Affix, name: '固钉' },
         ],
